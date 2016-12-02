31dee6a74cadec111ba0e08946a46a9961bd9aaf
PeteA <peter@Peters-MacBook-Pro.local>
2016-12-02 11:34:42 +0000

    Initial form with radio button

--- app/uk/gov/hmrc/offpayroll/controllers/HelloWorld.scala
+++ app/uk/gov/hmrc/offpayroll/controllers/HelloWorld.scala
@@ -1,16 +1,62 @@
+/*
+ * Copyright 2016 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.gov.hmrc.offpayroll.controllers
 
 import uk.gov.hmrc.play.frontend.controller.FrontendController
 import play.api.mvc._
+
 import scala.concurrent.Future
 import play.api.Play.current
+import play.api.data._
+import play.api.data.Forms._
 import play.api.i18n.Messages.Implicits._
 
 
 object HelloWorld extends HelloWorld
 
 trait HelloWorld extends FrontendController {
+
   val helloWorld = Action.async { implicit request =>
-		Future.successful(Ok(uk.gov.hmrc.offpayroll.views.html.helloworld.hello_world()))
+    val userForm = Form(
+      single(
+        "personalService.workerSentActualSubstitiute" -> boolean
+
+      )
+    )
+    Future.successful(Ok(uk.gov.hmrc.offpayroll.views.html.helloworld.hello_world(userForm)))
+  }
+
+  val helloWorldSubmit = Action.async { implicit request =>
+    val userForm = Form(
+      single(
+        "personalService.workerSentActualSubstitiute" -> boolean
+
+      )
+    )
+    Future.successful(userForm.bindFromRequest.fold(
+      formWithErrors => {
+        // binding failure, you retrieve the form containing errors:
+        BadRequest(uk.gov.hmrc.offpayroll.views.html.helloworld.hello_world(formWithErrors))
+      },
+      value => {
+        /* binding success, you get the actual value. */
+        Ok("value: " + value)
+      }
+    ))
+
   }
 }
