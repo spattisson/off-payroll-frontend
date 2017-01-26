0a7c0505daf9a6ef087ee776a400ce052bb677d8
Pete Anning <peter.anning@gmail.com>
2017-01-26 15:59:39 +0000

    Implemented Interview Session State

---
+++ test/uk/gov/hmrc/offpayroll/util/InterviewSessionHelperSpec.scala
@@ -0,0 +1,42 @@
+package uk.gov.hmrc.offpayroll.util
+
+import org.scalatest.{FlatSpec, Matchers}
+import play.api.mvc.Session
+import uk.gov.hmrc.offpayroll.util.InterviewSessionHelper.INTERVIEW_KEY
+
+/**
+  * Created by peter on 26/01/2017.
+  */
+class InterviewSessionHelperSpec extends FlatSpec with Matchers {
+
+  val mockSession = Session.deserialize(Map())
+
+  "An InterviewSessionHelper " should "take an existing Session and add a new value on an interview " in {
+
+    val newSession = InterviewSessionHelper.addValue(mockSession, "someQuestionTag", "someAnswer")
+
+    //println(newSession)
+    newSession.data.keys should contain(INTERVIEW_KEY)
+
+
+  }
+
+  it should "be able to add a second value" in {
+    val newSession  = InterviewSessionHelper.addValue(mockSession, "someQuestionTag", "someAnswer")
+    val finalSession = InterviewSessionHelper.addValue(newSession, "someOtherQuestionTag", "someAnswer")
+
+    finalSession.data.keys should contain(INTERVIEW_KEY)
+    finalSession(INTERVIEW_KEY) shouldBe "someQuestionTag:someAnswer;someOtherQuestionTag:someAnswer"
+
+  }
+
+  it should "replace the value of an existing key" in {
+    val newSession = InterviewSessionHelper.addValue(mockSession, "someQuestionTag", "someAnswer")
+    val finalSession = InterviewSessionHelper.addValue(newSession, "someQuestionTag", "someOtherAnswer")
+
+    finalSession(INTERVIEW_KEY) shouldBe "someQuestionTag:someOtherAnswer"
+
+  }
+
+
+}
