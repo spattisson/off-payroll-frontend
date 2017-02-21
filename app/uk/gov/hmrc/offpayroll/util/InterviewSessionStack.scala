05693caca882dce4f0ed0c73b5818c71b43b0bec
miloszm <milosz.muszynski@digital.hmrc.gov.uk>
2017-02-21 16:06:00 +0000

    test for interview session stack plus fixes

--- app/uk/gov/hmrc/offpayroll/util/InterviewSessionStack.scala
+++ app/uk/gov/hmrc/offpayroll/util/InterviewSessionStack.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.gov.hmrc.offpayroll.util
 
 import play.api.mvc.Session
@@ -7,19 +23,19 @@ object InterviewSessionStack {
   val INTERVIEW_KEY = "interview"
 
   def push(session: Session, values: List[String], element: Element): Session = {
-    val compressed = StringEncodedMap(session.data.get(INTERVIEW_KEY).getOrElse("")).asString
+    val compressed = session.data.get(INTERVIEW_KEY).getOrElse("")
     val newInterview = InterviewStack.push(CompressedInterview(compressed), values, element)
     session + (INTERVIEW_KEY -> newInterview.str)
   }
 
   def pop(session: Session, element: Element): (Session, List[String]) = {
-    val compressed = StringEncodedMap(session.data.get(INTERVIEW_KEY).getOrElse("")).asString
+    val compressed = session.data.get(INTERVIEW_KEY).getOrElse("")
     val (newInterview, values) = InterviewStack.pop(CompressedInterview(compressed), element)
     (session + (INTERVIEW_KEY -> newInterview.str), values)
   }
 
   def peek(session: Session, element: Element): (Session, List[String]) = {
-    val compressed = StringEncodedMap(session.data.get(INTERVIEW_KEY).getOrElse("")).asString
+    val compressed = session.data.get(INTERVIEW_KEY).getOrElse("")
     val (_, values) = InterviewStack.peek(CompressedInterview(compressed), element)
     (session + (INTERVIEW_KEY -> compressed), values)
   }
