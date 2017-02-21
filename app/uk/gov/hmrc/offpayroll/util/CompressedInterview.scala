06eb71ec37b21873572b489ceb8c43d3fbde9d35
miloszm <milosz.muszynski@digital.hmrc.gov.uk>
2017-02-21 10:29:46 +0000

    base62 encoder/decoder with tests

--- app/uk/gov/hmrc/offpayroll/util/CompressedInterview.scala
+++ app/uk/gov/hmrc/offpayroll/util/CompressedInterview.scala
@@ -24,29 +24,3 @@ case class CompressedInterview(str: String){
   def asPairs: List[(Int,Int)] = ???
 }
 
-
-object Base62EncoderDecoder {
-  val base = 62
-  val baseString: String = ((0 to 9) ++ ('A' to 'Z') ++ ('a' to 'z')).mkString
-
-  def encode(i: Long): String = {
-
-    @scala.annotation.tailrec
-    def div(i: Long, res: List[Int] = Nil): List[Int] = {
-      (i / base) match {
-        case q if q > 0 => div(q, (i % base).toInt :: res)
-        case _ => i.toInt :: res
-      }
-    }
-
-    div(i).map(x => baseString(x)).mkString
-  }
-
-  def decode(s: String): Long = {
-    s.zip(s.indices.reverse)
-      .map {
-        case (c, p) => baseString.indexOf(c) * scala.math.pow(base, p).toLong
-      }
-      .sum
-  }
-}
