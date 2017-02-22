1f047300627d99b9012e89c57c8d8aa6a2252775
miloszm <milosz.muszynski@digital.hmrc.gov.uk>
2017-02-22 10:08:11 +0000

    improved bit to answer conversion

--- test/uk/gov/hmrc/offpayroll/util/CompressedInterviewSpec.scala
+++ test/uk/gov/hmrc/offpayroll/util/CompressedInterviewSpec.scala
@@ -66,6 +66,11 @@ class CompressedInterviewSpec extends FlatSpec with Matchers {
   it should "provide map from question tags to values for an empty interview" in {
     val interview = CompressedInterview("")
     val values = interview.asMap
-    values.size shouldBe interview.asValueWidthPairs.size
+    val allAnswersEmpty = values.toList.forall{ case (_,a) => a.isEmpty }
+    allAnswersEmpty shouldBe true
   }
+
+
+
 }
+
